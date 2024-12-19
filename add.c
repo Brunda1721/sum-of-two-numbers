014b5d429d384996e34a739a5a80a8fa5e1f5405
Brunda1721 <brundab53@gmail.com>
2024-12-19 12:10:56 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  int num1,num2,sum;
+ printf("Enter any two numbers:");
+scanf("%d%D,&num1,&num2);
+  sum=num1+num2;
+  printf("The sum od %d and %d is %D ,num1,num2,sum);
+  return0;
+  }
